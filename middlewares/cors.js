815002d4fbf9966724f4e4c9befc4621457fd773
syirnik._. <Nadija.kirillova@yandex.ru>
2024-05-27 11:36:35 +0300

    <сообщение коммита>

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,17 +1,16 @@
-const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'localhost:3000'
-];
+// Вместо нашего адреса вставь свой
+// Можно ещё не удалять адреса с localhost, но мы удалили для краткости
+const allowedCors = ["https://frontend-domain.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
-    const { origin } = req.headers;
-    
-    if (allowedCors.includes(origin)) { // Если это наш друг
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-    
-    next();
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 
-module.exports = cors
+module.exports = cors;
